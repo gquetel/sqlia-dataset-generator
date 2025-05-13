244cc3eb58d4e5a61b9ddc19fa33412445a16647
gquetel <gregor.quetel@gmail.com>
2025-05-13 16:30:25 +0200

    chores: updates mysql-connector building mechanism

--- shell.nix
+++ shell.nix
@@ -9,13 +9,11 @@ let
       format = "wheel";
     in
     pkgs.python311.pkgs.buildPythonPackage {
-      # Have to use direct fetchurl as package is not updated in nixkpgs
-      # 9.3.0 or 9.2.0 are not available at https://files.pythonhosted.org/packages/source/m/mysql-connector-python/mysql-connector-python-9.3.0.tar.gz
-      # And i couldn't make fetchPypi use the correct wheel address.
+
       inherit pname version format;
-      src = pkgs.fetchurl {
-        url = "https://files.pythonhosted.org/packages/23/1d/8c2c6672094b538f4881f7714e5332fdcddd05a7e196cbc9eb4a9b5e9a45/mysql_connector_python-9.3.0-py2.py3-none-any.whl";
-        sha256 = "sha256-irdxnWFM9UY1IQgvq4avwhraUEtTgWYJDgDuqh/3Kbw=";
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-ixbVFEfjYD8YR4+1oZszO/tz+1j4cusFWhBWNfU9I0U="; 
       };
       doCheck = false;
     };
