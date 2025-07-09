62afd97f96772f5bcace6717ac735af96abd467a
gquetel <gregor.quetel@hydra>
2025-07-09 17:11:24 +0200

    chores: remove useless files

--- data/payloads/crawled/README.md
+++
@@ -1,7 +0,0 @@
-# Crawled malicious payloads
-
-This folder group small set of payloads crawled from different website, manually examined and labelled. 
-
-Sources: 
-- https://hacktricks.boitatech.com.br/pentesting/pentesting-mysql
-- https://github.com/sqlmapproject/sqlmap/blob/master/data/xml/queries.xml We extracted from the  "<dbms value="MySQL">" node the inband queries used to extract dbms information (users, privileges, tables, columns, ...)
--- models/training.py
+++ models/training.py
@@ -659,39 +659,39 @@ def train_models(
     # Train models and get their output.
     models = {}
 
-    # # We keep this one with scaling, it behaves way better.
-    # labels, scores = train_ocsvm_li(
-    #     df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True
-    # )
-    # models["Li and OCSVM"] = (labels, scores)
-    # labels, scores = train_lof_cv(df_train=df_train, df_test=df_test, df_val=df_val)
-    # models["CountVectorizer and LOF "] = (labels, scores)
+    # We keep this one with scaling, it behaves way better.
+    labels, scores = train_ocsvm_li(
+        df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True
+    )
+    models["Li and OCSVM"] = (labels, scores)
+    labels, scores = train_lof_cv(df_train=df_train, df_test=df_test, df_val=df_val)
+    models["CountVectorizer and LOF "] = (labels, scores)
 
-    # # We keep this one without scaler, it has the best results.
-    # labels, scores = train_ocsvm_cv(df_train=df_train, df_test=df_test, df_val=df_val)
-    # models["CountVectorizer and OCSVM"] = (labels, scores)
+    # We keep this one without scaler, it has the best results.
+    labels, scores = train_ocsvm_cv(df_train=df_train, df_test=df_test, df_val=df_val)
+    models["CountVectorizer and OCSVM"] = (labels, scores)
 
-    # # We keep this one without scaler, it has the best results.
-    # labels, scores = train_lof_li(df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True)
-    # models["Li and LOF"] = (labels, scores)
+    # We keep this one without scaler, it has the best results.
+    labels, scores = train_lof_li(df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True)
+    models["Li and LOF"] = (labels, scores)
 
     # AE is behaving way better with scaling
-    # labels, scores = train_ae_li(
-    #     df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True
-    # )
-    # models["Li and AE"] = (labels, scores)
+    labels, scores = train_ae_li(
+        df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=True
+    )
+    models["Li and AE"] = (labels, scores)
 
-    # # AE is behaving way better with scaling
-    # labels, scores = train_ae_cv(
-    #     df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=False
-    # )
-    # models["CountVectorizer and AE"] = (labels, scores)
+    # AE is behaving way better with scaling
+    labels, scores = train_ae_cv(
+        df_train=df_train, df_test=df_test, df_val=df_val, use_scaler=False
+    )
+    models["CountVectorizer and AE"] = (labels, scores)
 
-    # labels, scores = train_ocsvm_sbert(df_train=df_train, df_test=df_test, df_val=df_val)
-    # models["SBERT and OCSVM"] = (labels, scores)
+    labels, scores = train_ocsvm_sbert(df_train=df_train, df_test=df_test, df_val=df_val)
+    models["SBERT and OCSVM"] = (labels, scores)
 
-    # labels, scores = train_lof_sbert(df_train=df_train, df_test=df_test, df_val=df_val)
-    # models["SBERT and LOF"] = (labels, scores)
+    labels, scores = train_lof_sbert(df_train=df_train, df_test=df_test, df_val=df_val)
+    models["SBERT and LOF"] = (labels, scores)
 
     labels, scores = train_ae_sbert(df_train=df_train, df_test=df_test, df_val=df_val)
     models["SBERT and AE"] = (labels, scores)
@@ -741,8 +741,6 @@ if __name__ == "__main__":
 
     df = pd.read_csv(
         project_paths.dataset_path,
-        # "/home/gquetel/repos/sqlia-dataset/dataset.csv",
-        # dtype is specified to prevent a DtypeWarning
         dtype={
             "full_query": str,
             "label": int,
@@ -762,7 +760,6 @@ if __name__ == "__main__":
     if args.on_user_inputs:
         preprocess_for_user_inputs_training(df=df)
 
-    # df = df.sample(int(len(df) / 10), random_state=GENERIC.RANDOM_SEED)
     _df_train = df[df["split"] == "train"]
     df_train, df_val = train_test_split(
         _df_train,
