6ec2dc213a3806207e08ec083c03252e3fd9665a
gquetel <quetel.gregor@gmail.com>
2026-02-09 17:24:25 +0100

    fix(experiments): Fix xps parameters

--- scripts/evaluate_matrix_ae_li.sh
+++ scripts/evaluate_matrix_ae_li.sh
@@ -11,7 +11,7 @@ MODELS_DIR=./models/output/models/
 RESULTS_DIR=./models/output/
 
 # Evaluate ae_li_ABC on all test datasets
-python3 experiments/evaluate_model.py --model-path=$MODELS_DIR/ae_li_ABC.pth --model-type=ae_li --test-dataset=$DATASETS_DIR/generic-AdventureWorks.csv --output-dir=$RESULTS_DIR/ae_li_ABC_on_A/ 
+python3 experiments/evaluate_model.py --model-path=$MODELS_DIR/ae_li_ABC.pth --model-type=ae_li --test-dataset=$DATASETS_DIR/generic-OurAirports.csv --output-dir=$RESULTS_DIR/ae_li_ABC_on_A/ 
 python3 experiments/evaluate_model.py --model-path=$MODELS_DIR/ae_li_ABC.pth --model-type=ae_li --test-dataset=$DATASETS_DIR/generic-sakila.csv --output-dir=$RESULTS_DIR/ae_li_ABC_on_B/  
 python3 experiments/evaluate_model.py --model-path=$MODELS_DIR/ae_li_ABC.pth --model-type=ae_li --test-dataset=$DATASETS_DIR/generic-AdventureWorks.csv --output-dir=$RESULTS_DIR/ae_li_ABC_on_C/ 
 python3 experiments/evaluate_model.py --model-path=$MODELS_DIR/ae_li_ABC.pth --model-type=ae_li --test-dataset=$DATASETS_DIR/generic-OHR.csv --output-dir=$RESULTS_DIR/ae_li_ABC_on_D/ 
